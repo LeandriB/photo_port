9a9d988acbc1475bdf4eec8bd9b9d9a2cfa7e995
Leandri Beavis <leandrikuyk@gmail.com>
2022-08-16 19:45:56 -0400

    create Nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import About from './components/About';
+import Nav from './components/Nav';
 
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
